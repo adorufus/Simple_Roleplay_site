4fb0484d43f01ae2ebb3ff7ea19d90ff049bc6c2
adorufus <adorufus@gmail.com>
2016-06-24 21:47:20 +0700

    Create phpmailer/exception.php

---
+++ classes/phpmailer/exception.php
@@ -0,0 +1,7 @@
+<?php
+Class PhpMailerException extends \Exception{
+   public function errorMessage(){
+      $errorMsg = '<strong>' .this -> getMessage. "</strong><br/>\n";
+      echo $errorMsg;
+   }
+}
